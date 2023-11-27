1c5bc5ebc1b525d6b8c12c2af12528ba9eca2e67
doroved <dorroved@gmail.com>
2023-11-27 06:53:21 +0300

    remove dmidecode_output

--- src/linux.rs
+++ src/linux.rs
@@ -12,17 +12,17 @@ pub(crate) fn get_mid_result() -> Result<String, MIDError> {
     let machine_output = run_shell_comand("cat", ["/etc/machine-id"])?;
     let machine_id = machine_output.trim().to_lowercase();
 
-    let dmidecode_output =
-        run_shell_comand("sudo", ["dmidecode", "-t", "system"]).unwrap_or("".into());
+    // let dmidecode_output =
+    //     run_shell_comand("sudo", ["dmidecode", "-t", "system"]).unwrap_or("".into());
 
     let mut result = Vec::new();
 
     result.push(machine_id);
 
-    if !dmidecode_output.is_empty() {
-        let targets = ["Serial Number", "UUID"];
-        parse_and_push(&dmidecode_output, &targets, &mut result);
-    }
+    // if !dmidecode_output.is_empty() {
+    //     let targets = ["Serial Number", "UUID"];
+    //     parse_and_push(&dmidecode_output, &targets, &mut result);
+    // }
 
     // if result.is_empty() {
     //     return Err(MIDError::ResultMidError);
