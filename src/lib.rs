8681d6e8e3839df60d17954504809ed918a33d86
doroved <dorroved@gmail.com>
2023-11-28 20:03:41 +0300

    add comments for crate doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! Get an encrypted unique MachineID for MacOS/Linux/Windows.
+//!
+//! ```
+//! let machine_id = mid::get("mykey").unwrap();
+//! ```
+
 mod errors;
 mod linux;
 mod macos;
@@ -14,6 +20,28 @@ use macos::get_mid_result;
 #[cfg(target_os = "windows")]
 use windows::get_mid_result;
 
+/// Gets unique platform metrics and returns a `Result`, which can be a MID hash (SHA-256) or a `MIDError`.
+///
+/// # Errors
+///
+/// Returns [`Err`] if an error occurred while creating the MachineID.
+///
+/// # Examples
+///
+/// ```
+/// fn get_machine_id() -> Result<String, String> {
+/// 	// Generate a key on macOS/Linux with: openssl rand -hex 32
+/// 	let key = "293273abaf6fcb31d4a9b47b70a20b21133ff08852834c52c5f42ed8153b274a";
+///
+/// 	match mid::get(key) {
+/// 			Ok(mid) => Ok(mid),
+/// 			Err(err) => {
+/// 					println!("MID error: {}", err.to_string());
+/// 					Err(err.to_string())
+/// 			}
+/// 	}
+/// }
+/// ```
 pub fn get(key: &str) -> Result<String, MIDError> {
     match get_mid_result() {
         Ok(mid) => {
